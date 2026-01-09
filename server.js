28c57f68d475a9a907edf265d8b655d8ba6026e4
masterzhizni3-a11y <masterzhizni3@gmail.com>
2026-01-09 22:58:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
   res.end("Мой первый сервер работает!");
 });
 
